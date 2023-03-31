f80f48d1f1442a9d5e252d622298148c66010a8d
RafaelPCoelho <rafaelfipcoelho@gmail.com>
2023-03-31 17:22:09 -0300

    Configurações e adições para a rede teste HyperLedger Fabric

---
+++ workload/getAllAssets.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+class MyWorkload extends WorkloadModuleBase {
+    constructor() {
+        super();
+    }
+
+    async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+        await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+
+        /* for (let i=0; i<this.roundArguments.assets; i++) {
+            const assetID = `${this.workerIndex}_${i}`;
+            console.log(`Worker ${this.workerIndex}: Creating asset ${assetID}`);
+            const request = {
+                contractId: this.roundArguments.contractId,
+                contractFunction: 'CreateAsset',
+                invokerIdentity: 'User1',
+                contractArguments: [assetID,'blue','20','penguin','500'],
+                readOnly: false
+            };
+
+            await this.sutAdapter.sendRequests(request);
+        } */
+    }
+
+    async submitTransaction() {
+             const allMyArgs = {
+            contractId: this.roundArguments.contractId,
+            contractFunction: 'GetAllAssets',
+            invokerIdentity: 'User1',
+            readOnly: true
+        }
+
+        await this.sutAdapter.sendRequests(allMyArgs);
+    }
+
+    async cleanupWorkloadModule() {
+        for (let i=0; i<this.roundArguments.assets; i++) {
+            const assetID = `${this.workerIndex}_${i}`;
+            console.log(`Worker ${this.workerIndex}: Deleting asset ${assetID}`);
+            const request = {
+                contractId: this.roundArguments.contractId,
+                contractFunction: 'DeleteAsset',
+                invokerIdentity: 'User1',
+                contractArguments: [assetID],
+                readOnly: false
+            };
+
+            await this.sutAdapter.sendRequests(request);
+        }
+    }
+}
+
+function createWorkloadModule() {
+    return new MyWorkload();
+}
+
+module.exports.createWorkloadModule = createWorkloadModule;
